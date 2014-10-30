f9370bd282003d36ba54e255a281e9dd72bfa2c3
helenlam100 <helenlam@Helens-MacBook-Air.local>
2014-10-30 15:22:32 -0700

    initial commit

---
+++ ageist.rb
@@ -0,0 +1,19 @@
+age = rand(110).to_s
+puts 'Age is ' + age
+if age <= 1
+  puts 'baby'
+elsif age < 10 && age > 1
+  puts 'child'
+elsif age.between(10, 12)
+  puts 'tween'
+elsif age.between(13, 19)
+  puts 'teenager'
+elsif age.between(20, 40)
+  puts 'adult'
+elsif age.between(40, 65)
+  puts 'middle age'
+elsif age.between(66, 100)
+  puts 'senior'
+else age >= 100
+  puts 'record breaking old'
+end
